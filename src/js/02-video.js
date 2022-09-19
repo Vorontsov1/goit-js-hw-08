56d444e966b30daf437ab73717018a1452389411
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-19 20:20:10 +0300

    chaged_package.json

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,15 @@
 import Player from '@vimeo/player';
 
 import { throttle } from 'lodash';
-  const timeKey = 'videoplayer-current-time';
+
   const iframe = document.querySelector('#vimeo-player');
   const player = new Vimeo.Player(iframe);
 
   player.on('play', function () {
     console.log('played the video!');
   });
+
+    const timeKey = 'videoplayer-current-time';
 
   player.getVideoTitle().then(function (title) {
     console.log('title:', title);
