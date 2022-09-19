baadc45a4d578bc6e8ad37703b224ae1eccce429
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-19 20:56:34 +0300

    update_08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,15 @@
 import Player from '@vimeo/player';
 
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 
-  const iframe = document.querySelector('#vimeo-player');
-  const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
-  player.on('play', function () {
-    console.log('played the video!');
-  });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-    const timeKey = 'videoplayer-current-time';
-
-  player.getVideoTitle().then(function (title) {
-    console.log('title:', title);
-  });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
