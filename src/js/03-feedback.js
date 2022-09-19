b7ac7746672d78769435eb9ebd8c9b4871287abf
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-19 22:23:44 +0300

    start_task_03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(textRead, 500));
+form.addEventListener('submit', formSubmit);
