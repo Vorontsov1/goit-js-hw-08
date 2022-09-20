c86d51b68f8fcb32d219c16baf5eb23183fb2120
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-20 22:24:15 +0300

    finished_task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,4 +6,26 @@ const message = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('input', throttle(textRead, 500));
-form.addEventListener('submit', formSubmit);
+form.addEventListener('submit', onFormSubmit);
+
+function textRead(event) {
+    const dataStorage = {
+        email: email.value,
+        message: message.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault(); 
+    console.log(localStorage.getItem(STORAGE_KEY));
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    
+}
+saveFormData();
+function saveFormData() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    email.value = savedMessage.email || '';
+    message.value = savedMessage.message || '';
+}
