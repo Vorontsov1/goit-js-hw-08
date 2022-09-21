3bcc21f1925622e2c263c528d97ab1c65aed1d1e
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-21 21:53:02 +0300

    fixed_video_js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,12 +7,11 @@ const player = new Player(iframe);
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
-const onPlay = data => {
-  localStorage.setItem(STORAGE_KEY, data.seconds);
-  console.log(data.seconds);
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
 };
 
-player.on('timupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 const time = localStorage.getItem(STORAGE_KEY);
 if (time) {
   player.setCurrentTime(time);
